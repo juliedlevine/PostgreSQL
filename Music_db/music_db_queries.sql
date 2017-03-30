b846a481b7bb3ba2abfe0037c1f2baae5fde7a8f
juliemdyer <juliemdyer@gmail.com>
2017-03-30 14:03:23 -0400

    More exercises with Music database

--- Music_db/music_db_queries.sql
+++ Music_db/music_db_queries.sql
@@ -64,7 +64,22 @@ GROUP BY
 	artist.id;
 
 
--- 7. List all albums along with its total duration based on summing the duration of its tracks.
+-- 7. What is the title of each artist's latest album
+SELECT
+    distinct on (artist.id)
+    artist.name,
+    album.name,
+    album.year
+FROM
+    album,
+    artist
+WHERE
+    album.artist_id = artist.id
+ORDER BY
+	artist.id,
+	year desc;
+
+-- 8. List all albums along with its total duration based on summing the duration of its tracks.
 SELECT
 	album.name,
 	sum(track.length)
@@ -77,7 +92,7 @@ GROUP BY
 	album.id;
 
 
--- 8. What is the album with the longest duration?
+-- 9. What is the album with the longest duration?
 SELECT
     *
 FROM
@@ -96,7 +111,7 @@ ORDER BY
 LIMIT 1;
 
 
--- 9. Who are the 5 most prolific artists based on the number of albums they have recorded?
+-- 10. Who are the 5 most prolific artists based on the number of albums they have recorded?
 -- Top 5 artists based on number of albums
 SELECT
     artist.name,
@@ -113,7 +128,7 @@ LIMIT
     5;
 
 
--- 10. What are all the tracks a given artist has recorded?
+-- 11. What are all the tracks a given artist has recorded?
 SELECT
     artist.name,
     track.name
@@ -125,3 +140,145 @@ WHERE
 	track.album_id = album.id
 	and album.artist_id = artist.id
 	and artist.name = 'Belle & Sebastian';
+
+
+-- 12. What are the top 5 most often recorded songs?
+SELECT
+    song.name as song_name,
+    count(song.id)
+FROM
+    song,
+    track
+WHERE
+	track.song_id = song.id
+GROUP BY
+	song.id
+LIMIT
+	5;
+
+
+-- 13. Who are the top 5 song writers whose songs have been most often recorded?
+SELECT
+	song_writer.name as song_writer,
+	count(track.name) as track_name
+FROM
+	song_writer
+LEFT OUTER JOIN
+	song on song.song_writer_id = song_writer.id
+LEFT OUTER JOIN
+	track on track.song_id = song.id
+GROUP BY
+	song_writer.id
+LIMIT
+	5;
+
+
+-- 14. Who is the most prolific song writer based on the number of songs he has written?
+SELECT
+	song_writer.name as song_writer,
+	count(song.name) as song_count
+FROM
+	song_writer
+LEFT OUTER JOIN
+	song on song.song_writer_id = song_writer.id
+GROUP BY
+	song_writer
+ORDER BY
+	song_count desc
+LIMIT
+	1;
+
+
+-- 15. What songs has a given artist recorded?
+SELECT
+    distinct on (song.name)
+    artist.name as artist,
+    song.name
+FROM
+    artist
+LEFT OUTER JOIN
+    album on album.artist_id = artist.id
+LEFT OUTER JOIN
+    track on track.album_id = album.id
+LEFT OUTER JOIN
+    song on track.song_id = song.id
+WHERE
+	artist.name = 'Julie';
+
+
+-- 16. Who are the song writers whose songs a given artist has recorded?
+SELECT
+	song_writer.name as song_writer,
+	song.name as song_name,
+	artist.name as artist_name
+FROM
+    artist,
+    album,
+    track,
+    song,
+    song_writer
+WHERE
+    album.artist_id = artist.id
+    and track.album_id = album.id
+    and track.song_id = song.id
+	and song.song_writer_id = song_writer.id
+	and artist.name = 'Belle & Sebastian';
+
+
+-- 17. Who are the artists who have recorded a given song writer's songs?
+SELECT
+	distinct on (artist.name)
+	song_writer.name as song_writer,
+	song.name as song_name,
+	artist.name as artist_name
+FROM
+    artist,
+    album,
+    track,
+    song,
+    song_writer
+WHERE
+    album.artist_id = artist.id
+    and track.album_id = album.id
+    and track.song_id = song.id
+	and song.song_writer_id = song_writer.id
+	and song_writer.name = 'Giorgio';
+
+
+
+-- Bonus Challenge 1
+-- Given a lead artist, what collaborators has he worked with?
+SELECT
+	artist.name as Julie_collab_with
+FROM
+	(SELECT
+	    artist.name as lead_artist,
+	    album.name as album,
+	    collaborators.collaborator_id
+	FROM
+	    artist
+	LEFT OUTER JOIN
+		album on album.artist_id = artist.id
+	LEFT OUTER JOIN
+	    collaborators on collaborators.album_id = album.id
+	WHERE
+		artist.name = 'Julie') collab,
+	artist
+WHERE
+	artist.id = collab.collaborator_id;
+
+
+-- Super challenge: given an artist who has worked as a collaborator, who are the other collaborators he has worked with?
+SELECT
+    artist.name as lead_artist,
+    album.name as album,
+    collaborators.album_id,
+    collaborators.collaborator_id
+FROM
+    artist
+LEFT OUTER JOIN
+	album on album.artist_id = artist.id
+LEFT OUTER JOIN
+    collaborators on collaborators.album_id = album.id
+LEFT OUTER JOIN
+   artist on artist.id = collab.collaborator_id;
